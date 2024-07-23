cbe108ef5b60c612e8c418d6d29168d715461aaf
Bilenko Daria <bilenko-dasha@mail.ru>
2024-07-24 01:56:05 +0300

    Перенос открытитие браузера в фикстуры

--- conftest.py
+++ conftest.py
@@ -2,12 +2,13 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(autouse=True)
 def manage_browser():
 
     browser.config.window_height = 900 #фикстура - высота окна браузера
     browser.config.window_width = 900 #фикстура - ширина окна браузера
     browser.config.base_url = 'https://google.com'  # фикстура - дефолт браузер
+    browser.open('/ncr') #подтверждение cookie
     print("Браузер открыт фикстурой")
 
     yield
