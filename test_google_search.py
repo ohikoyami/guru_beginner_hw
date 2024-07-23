1624de796f55c01656d50e9130a250d0e1f9cfe6
Bilenko Daria <bilenko-dasha@mail.ru>
2024-07-24 01:56:49 +0300

    Исключение открытия браузера

--- test_google_search.py
+++ test_google_search.py
@@ -1,17 +1,12 @@
 import pytest
 from selene import browser, be, have
 
-def open_browser():
-    browser.open('/ncr')  #подтверждение cookie
 
-
-def test_search_1():
-    open_browser()
+def test_search_ok_ver():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_search_2():
-    open_browser()
+def test_search_bad_ver():
     browser.element('[name="q"]').should(be.blank).type('gfffddkk;kajjjfgggdgCXSJ').press_enter()
     browser.element('[id="botstuff"]').should(have.text('did not match any documents.'))
