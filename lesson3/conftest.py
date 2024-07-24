5154047eddce270e6de3c3c36e4e112439e5acda
Bilenko Daria <bilenko-dasha@mail.ru>
2024-07-24 22:53:19 +0300

    Перенос в папку Lesson3

--- lesson3/conftest.py
+++ lesson3/conftest.py
@@ -8,7 +8,7 @@ def manage_browser():
     browser.config.window_height = 900 #фикстура - высота окна браузера
     browser.config.window_width = 900 #фикстура - ширина окна браузера
     browser.config.base_url = 'https://google.com'  # фикстура - дефолт браузер
-    browser.open('/ncr') #подтверждение cookie
+    browser.open('/ncr') #подтверждение cookie + открытие браузера
     print("Браузер открыт фикстурой")
 
     yield
--- lesson3/test_google_search.py
+++ lesson3/test_google_search.py
@@ -1,7 +1,6 @@
 import pytest
 from selene import browser, be, have
 
-
 def test_search_ok_ver():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
