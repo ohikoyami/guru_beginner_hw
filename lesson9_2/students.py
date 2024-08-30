6e0e185eea6379bfb649b92a0282f4c813851a78
Bilenko Daria <bilenko-dasha@mail.ru>
2024-08-31 01:36:06 +0300

    добавление профиля студента

---
+++ lesson9_2/students.py
@@ -0,0 +1,18 @@
+from form_classes import Student
+
+daria = Student('Daria',
+                    'Bilenko',
+                    'e.mail@dssl.ru',
+                    '9876541234',
+                    'Maths',
+                    'st. Skobelevskaya',
+                    'Jaipur',
+                    'Rajasthan',
+                    'Female',
+                    9,
+                    4,
+                    2002,
+                    'Sports',
+                    'Music',
+                    'img.png'
+                    )
