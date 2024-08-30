07fea9085f3b67b13fc57422db194d0bcb4185bf
Bilenko Daria <bilenko-dasha@mail.ru>
2024-08-31 01:36:29 +0300

    реорганизация на основе правок

--- lesson9_2/test_form_fill_and_result.py
+++ lesson9_2/test_form_fill_and_result.py
@@ -1,35 +1,10 @@
-import os.path
-
-from selene import browser, have
-
-from form_classes import RegistrationPage
-from form_classes import CheckResultRegistrationPage
+from students import daria
 from form_classes import Student
+from form_classes import Application
 
 
 def test_fill_form():
-    daria = Student('Daria',
-                    'Bilenko',
-                    'e.mail@dssl.ru',
-                    '9876541234',
-                    'Maths',
-                    'st. Skobelevskaya',
-                    'Jaipur',
-                    'Rajasthan',
-                    'Female',
-                    9,
-                    4,
-                    2002,
-                    'Sports',
-                    'Music',
-                    'img.png'
-                    )
+    app = Application(daria)
 
-    registration_page = RegistrationPage(daria)
-    registration_result = CheckResultRegistrationPage()
-
-    registration_page.open_reg_page()
-    registration_page.fill_registration_page(daria)
-    registration_page.fill_birth_date(daria)
-    registration_page.submit_form()
-    registration_result.check_Result(daria)
+    app.registration.open_reg_page().fill_registration_page(daria).submit_form()
+    app.result.check_result(daria)
