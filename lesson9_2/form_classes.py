777d743540a81118eef09f216ac94ccf57eae244
Bilenko Daria <bilenko-dasha@mail.ru>
2024-08-31 01:35:44 +0300

    добавление application

--- lesson9_2/form_classes.py
+++ lesson9_2/form_classes.py
@@ -30,67 +30,70 @@ class RegistrationPage:
     def __init__(self, student: Student):
         self.first_name = browser.element('#firstName')
         self.last_name = browser.element('#lastName')
-        self.userEmail = browser.element('#userEmail')
-        self.chooseGender = browser.element('#genterWrapper').element(f'input[value={student.gender}]').element('..')
-        self.userNumber = browser.element('#userNumber')
-        self.subjectsInput = browser.element('#subjectsInput')
-        self.chooseHobbies1 = browser.element('#hobbies-checkbox-1').element('..')
-        self.chooseHobbies3 = browser.element('#hobbies-checkbox-3').element('..')
-        self.uploadPic = browser.element('#uploadPicture')
-        self.currentAddress = browser.element('#currentAddress')
-        self.State = browser.element('#react-select-3-input')
-        self.City = browser.element('#react-select-4-input')
-        self.resultBill = browser.element('#submit')
+        self.user_email = browser.element('#userEmail')
+        self.choose_gender = browser.element('#genterWrapper').element(f'input[value={student.gender}]').element('..')
+        self.user_number = browser.element('#userNumber')
+        self.subjects_input = browser.element('#subjectsInput')
+        self.choose_hobbies1 = browser.element('#hobbies-checkbox-1').element('..')
+        self.choose_hobbies3 = browser.element('#hobbies-checkbox-3').element('..')
+        self.upload_pic = browser.element('#uploadPicture')
+        self.current_address = browser.element('#currentAddress')
+        self.state = browser.element('#react-select-3-input')
+        self.city = browser.element('#react-select-4-input')
+        self.result_bill = browser.element('#submit')
 
     def open_reg_page(self):
         browser.open('/automation-practice-form')
         return self
 
-    def fill_birth_date(self, student: Student):
+    def fill_registration_page(self, student: Student):
+        self.first_name.type(student.name)
+        self.last_name.type(student.lastname)
+        self.user_email.type(student.email)
+        self.choose_gender.click()
+        self.user_number.type(student.phone)
         browser.element('#dateOfBirthInput').click()
         browser.element(
             f'.react-datepicker__month-select option[value="{student.bday_month - 1}"]').click()
         browser.element(f'.react-datepicker__year-select option[value="{student.bday_year}"]').click()
         browser.element(f'.react-datepicker__day'
                         f'.react-datepicker__day--{student.bday_day:03d}').click()
-        return self
-
-    def fill_registration_page(self, student: Student):
-        self.first_name.type(student.name)
-        self.last_name.type(student.lastname)
-        self.userEmail.type(student.email)
-        self.chooseGender.click()
-        self.userNumber.type(student.phone)
-        self.subjectsInput.type(student.subject1).press_tab()
-        self.chooseHobbies1.click()
-        self.chooseHobbies3.click()
-        self.uploadPic.send_keys(os.path.abspath(student.img))
-        self.currentAddress.type(student.addr)
-        self.State.type(student.state).press_tab()
-        self.City.type(student.city).press_tab()
+        self.subjects_input.type(student.subject1).press_tab()
+        self.choose_hobbies1.click()
+        self.choose_hobbies3.click()
+        self.upload_pic.send_keys(os.path.abspath(student.img))
+        self.current_address.type(student.addr)
+        self.state.type(student.state).press_tab()
+        self.city.type(student.city).press_tab()
         return self
 
     def submit_form(self):
-        self.resultBill.click()
+        self.result_bill.click()
         return self
 
 
-class CheckResultRegistrationPage:
+class ProfilePage:
     def __init__(self):
-        self.Result = browser.element('.modal-content').element('table').all('tr').all('td')
+        self.result = browser.element('.modal-content').element('table').all('tr').all('td').even
 
-    def check_Result(self, student: Student):
+    def check_result(self, student: Student):
         day = f'{student.bday_day:02d}'
         month = f'{calendar.month_name[student.bday_month]}'
 
-        assert self.Result.even.should(have.exact_texts(f'{student.name} {student.lastname}',
-                                                        student.email,
-                                                        student.gender,
-                                                        student.phone,
-                                                        f'{day} {month},{student.bday_year}',
-                                                        student.subject1,
-                                                        f'{student.hobbies1}, {student.hobbies2}',
-                                                        student.img,
-                                                        student.addr,
-                                                        f'{student.state} {student.city}'))
+        assert self.result.should(have.exact_texts(f'{student.name} {student.lastname}',
+                                                   student.email,
+                                                   student.gender,
+                                                   student.phone,
+                                                   f'{day} {month},{student.bday_year}',
+                                                   student.subject1,
+                                                   f'{student.hobbies1}, {student.hobbies2}',
+                                                   student.img,
+                                                   student.addr,
+                                                   f'{student.state} {student.city}'))
         return self
+
+
+class Application:
+    def __init__(self, student: Student):
+        self.registration = RegistrationPage(student)
+        self.result = ProfilePage()
