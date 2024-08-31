c876c6344f939a826ab58dfe013b8741ce052c19
Bilenko Daria <bilenko-dasha@mail.ru>
2024-08-31 20:21:46 +0300

    логин и пароль добавлены в .env

---
+++ lesson12/conftest.py
@@ -0,0 +1,46 @@
+import os
+
+import pytest
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
+
+@pytest.fixture(autouse=True)
+def manage_browser():
+    selenoid_user = os.getenv('SELENOID_USER')
+    selenoid_password = os.getenv('SELENOID_PASSWORD')
+
+    selenoid_user = 'user1'
+    selenoid_password = '1234'
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_user}:{selenoid_password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = 'https://demoqa.com'
+    driver.maximize_window()
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_source(browser)
+    attach.add_video(browser)
+
+    browser.quit()
+    print("Браузер закрыт фикстурой")
