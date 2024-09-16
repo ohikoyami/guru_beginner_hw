d39e7d038d4fd14da747235a6265f5f8563ad62a
Bilenko Daria <bilenko-dasha@mail.ru>
2024-09-16 21:23:37 +0300

    правки

--- lesson13/conftest.py
+++ lesson13/conftest.py
@@ -1,11 +1,36 @@
+import os
+
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from dotenv import load_dotenv
 
 
 @pytest.fixture(autouse=True)
 def manage_browser():
+    load_dotenv()
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 10
+    driver.maximize_window()
 
     yield browser
 
