f077a55ec8de685de664839864b3a9be7717eb28
Bilenko Daria <bilenko-dasha@mail.ru>
2024-09-18 02:58:26 +0300

    Выполнение 14 урока

---
+++ lesson14/conftest.py
@@ -0,0 +1,47 @@
+import os
+
+import pytest
+from selene import browser, command
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#from utils import attach
+from dotenv import load_dotenv
+
+
+@pytest.fixture(autouse=True)
+def manage_browser():
+    load_dotenv()
+    selenoid_user = os.getenv('SELENOID_USER')
+    selenoid_password = os.getenv('SELENOID_PASSWORD')
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_user}:{selenoid_password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = 'https://krispykreme-moskva.ru'
+    browser.config.timeout = 10
+    browser.open('/')
+    driver.maximize_window()
+
+    yield browser
+
+    '''attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_source(browser)
+    attach.add_video(browser)'''
+
+    browser.quit()
+    print("Браузер закрыт фикстурой")
---
+++ lesson14/test_imba_shop.py
@@ -0,0 +1,44 @@
+from selene import browser
+import allure
+from allure_commons.types import Severity
+from products.raspberry_pistachio_shell import raspberry_pistachio_shell
+from classes.PageClasses import ShopPonchiki
+
+shop_ponchiki = ShopPonchiki(raspberry_pistachio_shell)
+
+
+@allure.tag('web')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности сайта')
+@allure.step('Проверка тайтла главной страницы сайта')
+def test_open_base_page():
+    shop_ponchiki.base_page.open()
+
+
+@allure.step('Проверка лого главной страницы сайта')
+def test_logo_visibility():
+    shop_ponchiki.base_page.logo_visibility()
+
+
+@allure.step('Проверка открытия католога с пончиками')
+def test_ponchik_on_catalog_page():
+    browser.open('/ponchiki')
+    shop_ponchiki.catalog_page.check_ponchiki()
+
+
+@allure.step('Проверка добавления поника в корзину')
+def test_add_to_cart():
+    browser.open('/ponchiki')
+    shop_ponchiki.catalog_page.add_product_to_cart(raspberry_pistachio_shell)
+    shop_ponchiki.cart_page.check_cart(raspberry_pistachio_shell)
+
+
+@allure.step('Проверка добавления поника в избранное')
+def test_add_to_favorite():
+    browser.open('/ponchiki')
+    shop_ponchiki.catalog_page.open_product_card(raspberry_pistachio_shell)
+    shop_ponchiki.catalog_page.add_product_to_favorite_page()
+    shop_ponchiki.favorite_page.open()
+    shop_ponchiki.favorite_page.check_added_product(raspberry_pistachio_shell)
