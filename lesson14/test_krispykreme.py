88498ee441c93100d2f0a1baff51d675b9e63e25
Bilenko Daria <bilenko-dasha@mail.ru>
2024-09-18 03:47:42 +0300

    правки

--- lesson14/test_krispykreme.py
+++ lesson14/test_krispykreme.py
@@ -16,25 +16,41 @@ shop_ponchiki = ShopPonchiki(raspberry_pistachio_shell)
 def test_open_base_page():
     shop_ponchiki.base_page.open()
 
-
+@allure.tag('web')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности сайта')
 @allure.step('Проверка лого главной страницы сайта')
 def test_logo_visibility():
     shop_ponchiki.base_page.logo_visibility()
 
-
+@allure.tag('web')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности сайта')
 @allure.step('Проверка открытия католога с пончиками')
 def test_ponchik_on_catalog_page():
     browser.open('/ponchiki')
     shop_ponchiki.catalog_page.check_ponchiki()
 
-
+@allure.tag('web')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности сайта')
 @allure.step('Проверка добавления поника в корзину')
 def test_add_to_cart():
     browser.open('/ponchiki')
     shop_ponchiki.catalog_page.add_product_to_cart(raspberry_pistachio_shell)
     shop_ponchiki.cart_page.check_cart(raspberry_pistachio_shell)
 
-
+@allure.tag('web')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности сайта')
 @allure.step('Проверка добавления поника в избранное')
 def test_add_to_favorite():
     browser.open('/ponchiki')
