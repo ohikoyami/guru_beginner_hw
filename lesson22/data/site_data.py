23da961da18612bbea83a8f65ef589aeed2b38ce
Bilenko Daria <d.bilenko@dssl.ru>
2024-11-10 20:55:28 +0300

    Выполнение задания

--- lesson22/data/site_data.py
+++ lesson22/data/site_data.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 from typing import List
+
+from pydantic.utils import Optional
+
 from data.ponchik_info import Ponchik
 
 
 class CartResponse(BaseModel):
-    products: List[Ponchik]
+    products: Optional[List[Ponchik]] = None #Необязательное поле, может быть либо Ponchik, либо None
 
 
 class FavoriteProduct(BaseModel):
---
+++ lesson22/tests/api/test_api_krispykreme.py
@@ -0,0 +1,41 @@
+import allure
+import requests
+from allure_commons.types import Severity
+from pydantic import BaseModel
+
+from data.ponchik_info import Ponchik
+from products.raspberry_pistachio_shell import raspberry_pistachio_shell
+from classes.api.PageClasses_API import Cart, Favorite
+
+cart = Cart()
+favorite = Favorite()
+
+
+@allure.tag('api')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности корзины')
+@allure.title('Проверка добавления пончика в корзину и обновление количества')
+def test_cart_api(api_session):
+    with allure.step('Добавление пончика в корзину'):
+        cart.add_product_to_cart(raspberry_pistachio_shell, api_session)
+    with allure.step('Обновление количества пончика в корзине'):
+        cart.update_product_quantity(raspberry_pistachio_shell, api_session)
+    with allure.step('Получение данных корзины и проверка результата'):
+        cart.get_cart_data(api_session)
+
+
+@allure.tag('api')
+@allure.severity(Severity.MINOR)
+@allure.label('owner', 'bilenkoda')
+@allure.feature('Проверка сайта krispykreme')
+@allure.story('Тест функциональности избранного')
+@allure.title('Проверка добавления пончика в избранное и обновление избранного')
+def test_favorite_api(api_session):
+    with allure.step('Добавление пончика в избранное'):
+        favorite.add_in_favorite(raspberry_pistachio_shell, api_session)
+    with allure.step('Обновление пончика в избранном'):
+        favorite.update_favorite_product(raspberry_pistachio_shell, api_session)
+    with allure.step('Получение списка избранных продуктов и проверка результата'):
+        favorite.get_favorite_products(api_session)
