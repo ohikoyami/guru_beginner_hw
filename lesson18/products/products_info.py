1785ab50f97883c91a0f2862dff060192a28cd6f
Bilenko Daria <68212325+ohikoyami@users.noreply.github.com>
2024-12-02 12:41:31 +0300

    Update products_info.py

--- lesson18/products/products_info.py
+++ lesson18/products/products_info.py
@@ -1,4 +1,6 @@
+import logging
 import requests
+from allure_commons.types import AttachmentType
 from selene import browser, be, have, query
 from config.config import BASE_URL, LOGIN, PASSWORD
 
@@ -14,6 +16,20 @@ class Product:
         response = requests.post(url, cookies=auth_cookie())
         browser.driver.refresh()
         browser.element('.cart-qty').should(be.visible).should(have.text(f'({self.product_quantity})'))
+        attach(body=response.request.url, name='Request', attachment_type=AttachmentType.TEXT,
+                           extension="txt")
+        attach(body=json.dumps(dict(response.request.headers),
+                           indent=4, ensure_ascii=True), name='Request Headers',
+        attachment_type=AttachmentType.JSON,
+                           extension="json")
+        attach(body=json.dumps(response.json(),
+                           indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON,
+                           extension="json")
+        attach(body=str(response.cookies.get('Nop.customer')), name="Cookies", attachment_type=AttachmentType.TEXT,
+                           extension="txt")
+        logging.info(response.request.url)
+        logging.info(response.status_code)
+        logging.info(response.text)
         return response
 
 
